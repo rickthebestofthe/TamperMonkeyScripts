d23d1e7e89f8eb081b49e96aaf91cfa15d253a74
HOHOH134 <1992537+showchi@users.noreply.github.com>
2022-10-13 11:35:25 +0800

    Update WikipediaToWikiwand.user.js

--- WikipediaToWikiwand.user.js
+++ WikipediaToWikiwand.user.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name                Wikipedia to Wikiwand
+// @namespace           https://github.com/showchi/TamperMonkeyScripts
+// @version             0.1
 // @description         Redirect Wikipedia to Wikiwand for a modern browsing experience.
 // @description:zh-CN   重定向 Wikipedia 页面到 Wikiwand 以获得现代的浏览体验
 // @description:zh-TW   重定向 Wikipedia 頁面到 Wikiwand 以獲得現代的瀏覽體驗
 // @author              Jade Worths
-// @version             1.0.3
-// @namespace           https://github.com/showchi/TamperMonkeyScripts
+// @match               http*://*.wikipedia.org/*/*
 // @icon                https://www.wikiwand.com/favicon.ico
-// @include             http*://*.wikipedia.org/*/*
-// @run-at              document-start
+// @grant               none
 // ==/UserScript==
 
-var language=window.location.host.split('.')[0]
-window.location.replace(window.location.href.replace(/[^\s]*\.wikipedia\.org\/[\w-]+/, 'https://www.wikiwand.com/'+ language));
+(function() {
+    'use strict';
+
+    var language=window.location.host.split('.')[0]
+    window.location.replace(window.location.href.replace(/[^\s]*\.wikipedia\.org\/[\w-]+/, 'https://www.wikiwand.com/'+ language));
+})();
