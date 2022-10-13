76ebaa6dc5614bc9cfeb8b4160180c77119b7a28
HOHOH134 <1992537+showchi@users.noreply.github.com>
2022-10-13 10:45:57 +0800

    修复https://zh.m.wikipedia.org/*重定向到Wikiwand死循环

---
+++ WikipediaToWikiwand.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name                WikipediaToWikiwand
+// @description         Redirect Wikipedia to Wikiwand for a modern browsing experience.
+// @description:zh-CN   重定向 Wikipedia 页面到 Wikiwand 以获得现代的浏览体验
+// @description:zh-TW   重定向 Wikipedia 頁面到 Wikiwand 以獲得現代的瀏覽體驗
+// @version             1.0.3
+// @namespace           https://github.com/showchi/TamperMonkeyScripts
+// @icon                https://www.wikiwand.com/favicon.ico
+// @include             http*://*.wikipedia.org/*/*
+// @run-at              document-start
+// ==/UserScript==
+var language=navigator.language ? navigator.language.toLowerCase() : 'zh'
+window.location.replace(document.location.href.replace(/[^\s]*\.wikipedia\.org\/[\w-]+/, 'https://www.wikiwand.com/'+ language));
