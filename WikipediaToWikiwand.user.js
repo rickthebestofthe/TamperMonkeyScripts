ef10e2d552ce38e6ce114cccae794bf780a4cb75
HOHOH134 <1992537+showchi@users.noreply.github.com>
2022-10-14 11:05:48 +0800

    Update WikipediaToWikiwand.user.js

--- WikipediaToWikiwand.user.js
+++ WikipediaToWikiwand.user.js
@@ -8,6 +8,7 @@
 // @description:zh-CN   重定向 Wikipedia 页面到 Wikiwand 以获得现代的浏览体验
 // @description:zh-TW   重定向 Wikipedia 頁面到 Wikiwand 以獲得現代的瀏覽體驗
 // @author              Jade Worths
+// @license             MIT
 // @match               http*://*.wikipedia.org/*/*
 // @icon                https://www.wikiwand.com/favicon.ico
 // @grant               none
